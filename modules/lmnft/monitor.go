d227e067f30d5f5304ca564cf596df557a3f6e57
weeaa <github@ryanriviere.com>
2023-08-18 00:35:00 +0200

    Added new files for monitoring non-fungible tokens (NFT)
    Added two new files: `monitor.go` and `types.go` in the `lmnft` module. These files allow monitoring of non-fungible tokens (NFTs) across various blockchain networks. The monitor dispatches notifications on Discord whenever new NFTs meet the criteria specified in the search payload. This feature will help in quick identification of potential NFT investment opportunities.

---
+++ modules/lmnft/monitor.go
@@ -0,0 +1,264 @@
+package lmnft
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/charmbracelet/log"
+	"github.com/weeaa/nft/discord"
+	"github.com/weeaa/nft/handler"
+	"github.com/weeaa/nft/pkg/logger"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const moduleName = "LaunchMyNFT"
+const retryDelay = 2500 * time.Millisecond
+
+// Monitor monitors SOL Hype Mints by default.
+// If you want to monitor non SOL Mints, switch the "Solana" from the
+// payload to the network you want to monitor.
+func Monitor(client discord.Client, networks []Network, delay time.Duration) {
+
+	logger.LogStartup(moduleName)
+
+	h := handler.New()
+	t := &Webhook{}
+
+	var buf bytes.Buffer
+	var filterByQuery string
+
+	if len(networks) > 0 {
+		var filterByNetworks []string
+		for _, network := range networks {
+			filterByNetworks = append(filterByNetworks, string(network))
+		}
+		filterByQuery = fmt.Sprintf("&& type:=[`%s`]", strings.Join(filterByNetworks, "`,`"))
+	} else {
+		filterByQuery = fmt.Sprintf("type:=[`%s`]", networks[0])
+	}
+
+	payload := searchPayload{
+		Searches: Searches{
+			{
+				QueryBy:             "collectionName,owner",
+				PerPage:             10,
+				SortBy:              "lastMintedAt:desc",
+				HighlightFullFields: "collectionName,owner",
+				Collection:          "collections",
+				Q:                   "*",
+				FacetBy:             "soldOut,twitterVerified,type,cost",
+				FilterBy:            "soldOut:=[false] && twitterVerified:=[true] " + filterByQuery,
+				MaxFacetValues:      10,
+				Page:                3,
+			},
+			{
+				QueryBy:             "collectionName,owner",
+				PerPage:             10,
+				SortBy:              "lastMintedAt:desc",
+				HighlightFullFields: "collectionName,owner",
+				Collection:          "collections",
+				Q:                   "*",
+				FacetBy:             "soldOut",
+				FilterBy:            "twitterVerified:=[true] " + filterByQuery,
+				MaxFacetValues:      10,
+				Page:                1,
+			},
+			{
+				QueryBy:             "collectionName,owner",
+				PerPage:             10,
+				SortBy:              "lastMintedAt:desc",
+				HighlightFullFields: "collectionName,owner",
+				Collection:          "collections",
+				Q:                   "*",
+				FacetBy:             "twitterVerified",
+				FilterBy:            "soldOut:=[false] " + filterByQuery,
+				MaxFacetValues:      10,
+				Page:                1,
+			},
+			{
+				QueryBy:             "collectionName,owner",
+				PerPage:             10,
+				SortBy:              "lastMintedAt:desc",
+				HighlightFullFields: "collectionName,owner",
+				Collection:          "collections",
+				Q:                   "*",
+				FacetBy:             "type",
+				FilterBy:            "soldOut:=[false] && twitterVerified:=[true]",
+				MaxFacetValues:      10,
+				Page:                1,
+			},
+		},
+	}
+
+	if err := json.NewEncoder(&buf).Encode(payload); err != nil {
+		logger.LogError("", err)
+	}
+
+	go func() {
+		for {
+
+			//payload := strings.NewReader("{\"searches\":[{\"query_by\":\"collectionName,owner\",\"per_page\":10,\"sort_by\":\"lastMintedAt:desc\",\"highlight_full_fields\":\"collectionName,owner\",\"collection\":\"collections\",\"q\":\"*\",\"facet_by\":\"soldOut,twitterVerified,type,cost\",\"filter_by\":\"soldOut:=[false] && twitterVerified:=[true] && type:=[`Solana`]\",\"max_facet_values\":10,\"page\":3},{\"query_by\":\"collectionName,owner\",\"per_page\":10,\"sort_by\":\"lastMintedAt:desc\",\"highlight_full_fields\":\"collectionName,owner\",\"collection\":\"collections\",\"q\":\"*\",\"facet_by\":\"soldOut\",\"filter_by\":\"twitterVerified:=[true] && type:=[`Solana`]\",\"max_facet_values\":10,\"page\":1},{\"query_by\":\"collectionName,owner\",\"per_page\":10,\"sort_by\":\"lastMintedAt:desc\",\"highlight_full_fields\":\"collectionName,owner\",\"collection\":\"collections\",\"q\":\"*\",\"facet_by\":\"twitterVerified\",\"filter_by\":\"soldOut:=[false] && type:=[`Solana`]\",\"max_facet_values\":10,\"page\":1},{\"query_by\":\"collectionName,owner\",\"per_page\":10,\"sort_by\":\"lastMintedAt:desc\",\"highlight_full_fields\":\"collectionName,owner\",\"collection\":\"collections\",\"q\":\"*\",\"facet_by\":\"type\",\"filter_by\":\"soldOut:=[false] && twitterVerified:=[true]\",\"max_facet_values\":10,\"page\":1}]}")
+
+			req := &http.Request{
+				Method: http.MethodPost,
+				URL:    &url.URL{Scheme: "https://", Host: "s.launchmynft.io", Path: "/multi_search?x-typesense-api-key=UkN4Vnd3V2JMWWVIRlFNcTJ3dng4VGVtMGtvVGxBcmJJTTFFYS9MNXp1WT1Ha3dueyJmaWx0ZXJfYnkiOiJoaWRkZW46ZmFsc2UiLCJleGNsdWRlX2ZpZWxkcyI6ImhpZGRlbiIsInF1ZXJ5X2J5IjoiY29sbGVjdGlvbk5hbWUsb3duZXIiLCJsaW1pdF9oaXRzIjoyMDAsInNuaXBwZXRfdGhyZXNob2xkIjo1MH0%3D"},
+				Body:   io.NopCloser(&buf),
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Error(err)
+				time.Sleep(retryDelay)
+				continue
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Error(err)
+				time.Sleep(retryDelay)
+				return
+			}
+
+			var res resLaunchMyNFT
+			err = json.NewDecoder(bytes.NewBuffer(body)).Decode(&res)
+			if err != nil {
+				log.Error(err)
+				time.Sleep(retryDelay)
+				return
+			}
+
+			for i := 0; i < len(res.Results); i++ {
+				t.Name = res.Results[i].Hits[i].Document.CollectionName
+				t.Fraction = res.Results[i].Hits[i].Document.FractionMinted * 100
+				t.Cost = res.Results[i].Hits[i].Document.Cost
+				t.Supply = res.Results[i].Hits[i].Document.MaxSupply
+				t.TotalMinted = res.Results[i].Hits[i].Document.TotalMints
+				t.Image = res.Results[i].Hits[i].Document.CollectionCoverURL
+				t.Description = res.Results[i].Hits[i].Document.Description
+				t.MintLink = "https://www.launchmynft.io/collections/" + res.Results[i].Hits[i].Document.Owner + "/" + res.Results[i].Hits[i].Document.ID
+
+				switch res.Results[i].Hits[i].Document.Type {
+				case string(Solana):
+					t.CMID, t.Discord, t.Twitter = scrapeCMID(t.MintLink)
+					t.Secondary = "https://hyperspace.xyz/collection/" + t.CMID
+					if t.Twitter == "" {
+						t.Twitter = "no account :("
+					} else if t.Discord == "" {
+						t.Discord = "no server :("
+					} else {
+						t.Discord = "[Server](https://discord.gg/" + t.Discord + ")"
+						t.Twitter = "[Account](https://twitter.com/" + t.Twitter + ")"
+					}
+				case string(Polygon):
+				case string(Ethereum):
+				case string(Binance):
+				case string(Aptos):
+				case string(Avalanche):
+				case string(Fantom):
+				case string(Stacks):
+				default:
+					logger.LogError(moduleName, errors.New("unknown network"))
+				}
+
+				h.M.Set(t.Name, t.TotalMinted)
+
+				if h.M.Get(t.Name) == h.MCopy.Get(t.Name) {
+					time.Sleep(40 * time.Second)
+					continue
+				}
+
+				if t.Fraction >= 6 && t.TotalMinted >= 100 && t.CMID != "" {
+					if err = client.LaunchMyNFTNotification(t); err != nil {
+						logger.LogError(moduleName, err)
+					}
+
+					log.Info("Collection Found!", "name", t.Name, "fraction", t.Fraction, "totalMinted", t.TotalMinted, "network", t.Network)
+				} else {
+					log.Warn("Collection 2 Low :(", "name", t.Name, "fraction", t.Fraction, "totalMinted", t.TotalMinted, "network", t.Network)
+				}
+
+				h.M.ForEach(func(k string, v interface{}) {
+					h.MCopy.Set(k, v)
+				})
+
+			}
+
+			time.Sleep(delay)
+		}
+	}()
+}
+
+// used for SOL
+func scrapeCMID(input string) (string, string, string) {
+
+	defaultVal := "–"
+
+	candymachineUrl, err := url.Parse(input)
+	if err != nil {
+		log.Errorf("lmnft.scrapeCMID: ERROR Parsing cmidURL [%w]", err)
+	}
+
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL:    candymachineUrl,
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Errorf("lmnft.ScrapeCMID: ERROR Client [%w]", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("lmnft.ScrapeCMID: ERROR Reading Body Response [%w]", err)
+		return defaultVal, defaultVal, defaultVal
+	}
+
+	var res resSolana
+	switch resp.StatusCode {
+	case 400:
+	default:
+		return defaultVal, defaultVal, defaultVal
+	}
+
+	//var res resCMID
+	doc, errDoc := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	if errDoc != nil {
+		log.Errorf("lmnft.ScrapeCMID: ERROR Decoding JSON Script [%w]", err)
+		return defaultVal, defaultVal, defaultVal
+	}
+
+	candy := doc.Find("script[id=__NEXT_DATA__]").Text()
+
+	err = json.NewDecoder(strings.NewReader(candy)).Decode(&res)
+	if err != nil {
+		log.Errorf("lmnft.ScrapeCMID: ERROR Decoding JSON Script [%w]", err)
+		return defaultVal, defaultVal, defaultVal
+	}
+
+	return res.Props.PageProps.Collection.NewCandyMachineAccountID, res.Props.PageProps.Collection.Discord, res.Props.PageProps.Collection.Twitter
+
+}
+
+type searchPayload struct {
+	Searches Searches `json:"searches"`
+}
+
+type Searches []struct {
+	QueryBy             string `json:"query_by"`
+	PerPage             int    `json:"per_page"`
+	SortBy              string `json:"sort_by"`
+	HighlightFullFields string `json:"highlight_full_fields"`
+	Collection          string `json:"collection"`
+	Q                   string `json:"q"`
+	FacetBy             string `json:"facet_by"`
+	FilterBy            string `json:"filter_by"`
+	MaxFacetValues      int    `json:"max_facet_values"`
+	Page                int    `json:"page"`
+}
---
+++ modules/lmnft/types.go
@@ -0,0 +1,342 @@
+package lmnft
+
+type Network string
+
+const (
+	Solana    Network = "Solana"
+	Ethereum  Network = "Ethereum"
+	Polygon   Network = "Polygon"
+	Binance   Network = "Binance"
+	Aptos     Network = "Aptos"
+	Avalanche Network = "Avalanche"
+	Fantom    Network = "Fantom"
+	Stacks    Network = "Stacks"
+)
+
+type Webhook struct {
+	Name            string
+	Description     string
+	Image           string
+	MintLink        string
+	CMID            string
+	Supply          int
+	TotalMinted     int
+	Cost            float64
+	Fraction        float64
+	Verified        bool
+	Twitter         string
+	Discord         string
+	Secondary       string
+	Network         string
+	ContractAddress string // for other networks than SOL
+}
+
+type resLaunchMyNFT struct {
+	Results []struct {
+		FacetCounts []struct {
+			Counts []struct {
+				Count       int    `json:"count"`
+				Highlighted string `json:"highlighted"`
+				Value       string `json:"value"`
+			} `json:"counts"`
+			FieldName string `json:"field_name"`
+			Stats     struct {
+				TotalValues int `json:"total_values"`
+			} `json:"stats,omitempty"`
+			Stats0 struct {
+				Avg         float64 `json:"avg"`
+				Max         float64 `json:"max"`
+				Min         float64 `json:"min"`
+				Sum         float64 `json:"sum"`
+				TotalValues int     `json:"total_values"`
+			} `json:"stats,omitempty"`
+		} `json:"facet_counts"`
+		Found int `json:"found"`
+		Hits  []struct {
+			Document struct {
+				CollectionBannerURL string  `json:"collectionBannerUrl"`
+				CollectionCoverURL  string  `json:"collectionCoverUrl"`
+				CollectionName      string  `json:"collectionName"`
+				Cost                float64 `json:"cost"`
+				Deployed            int64   `json:"deployed"`
+				Description         string  `json:"description"`
+				FractionMinted      float64 `json:"fractionMinted"`
+				ID                  string  `json:"id"`
+				LastMintedAt        int64   `json:"lastMintedAt"`
+				LaunchDate          int64   `json:"launchDate"`
+				MaxSupply           int     `json:"maxSupply"`
+				Owner               string  `json:"owner"`
+				SoldOut             bool    `json:"soldOut"`
+				TotalMints          int     `json:"totalMints"`
+				TwitterVerified     bool    `json:"twitterVerified"`
+				Type                string  `json:"type"`
+			} `json:"document,omitempty"`
+		} `json:"hits"`
+		OutOf         int `json:"out_of"`
+		Page          int `json:"page"`
+		RequestParams struct {
+			CollectionName string `json:"collection_name"`
+			PerPage        int    `json:"per_page"`
+			Q              string `json:"q"`
+		} `json:"request_params"`
+		SearchCutoff bool `json:"search_cutoff"`
+		SearchTimeMs int  `json:"search_time_ms"`
+	} `json:"results"`
+}
+
+// todo: add all possible responses
+type resSolana struct {
+	Props struct {
+		PageProps struct {
+			Collection struct {
+				CollectionName           string      `json:"collectionName"`
+				HeartCount               int         `json:"heartCount"`
+				MintedLast30Mins         bool        `json:"mintedLast30mins"`
+				Twitter                  string      `json:"twitter"`
+				Version                  int         `json:"version"`
+				Tx                       string      `json:"tx"`
+				LaunchDate               int64       `json:"launchDate"`
+				MetadataCID              string      `json:"metadataCID"`
+				Hidden                   bool        `json:"hidden"`
+				Cost                     string      `json:"cost"`
+				TotalMints               int         `json:"totalMints"`
+				HasSoldEnough            bool        `json:"hasSoldEnough"`
+				LaunchLater              bool        `json:"launchLater"`
+				NewCandyMachineAccountID string      `json:"newCandyMachineAccountId"`
+				CollectionCoverURL       string      `json:"collectionCoverUrl"`
+				LastMintedAt             int64       `json:"lastMintedAt"`
+				MeSymbol                 string      `json:"meSymbol"`
+				SoldOut                  bool        `json:"soldOut"`
+				Description              string      `json:"description"`
+				FractionMinted           float64     `json:"fractionMinted"`
+				Discord                  string      `json:"discord"`
+				HiddenMetadataCID        interface{} `json:"hiddenMetadataCID"`
+				Deployed                 int64       `json:"deployed"`
+				TwitterVerified          bool        `json:"twitterVerified"`
+				MaxSupply                int         `json:"maxSupply"`
+				Type                     string      `json:"type"`
+				Owner                    string      `json:"owner"`
+			} `json:"collection"`
+		} `json:"pageProps"`
+		NSsp bool `json:"__N_SSP"`
+	} `json:"props"`
+	Page  string `json:"page"`
+	Query struct {
+		Userid       string `json:"userid"`
+		Collectionid string `json:"collectionid"`
+	} `json:"query"`
+	BuildID      string        `json:"buildId"`
+	IsFallback   bool          `json:"isFallback"`
+	Gssp         bool          `json:"gssp"`
+	ScriptLoader []interface{} `json:"scriptLoader"`
+}
+
+type resEthereum struct {
+	PageProps struct {
+		Collection struct {
+			WhitelistCost string  `json:"whitelistCost"`
+			Owner         string  `json:"owner"`
+			Cost          float64 `json:"cost"`
+			Address       string  `json:"address"`
+			Hidden        bool    `json:"hidden"`
+			Abi           []struct {
+				Inputs []struct {
+					Name         string `json:"name"`
+					InternalType string `json:"internalType"`
+					Type         string `json:"type"`
+					Indexed      bool   `json:"indexed,omitempty"`
+				} `json:"inputs,omitempty"`
+				StateMutability string `json:"stateMutability,omitempty"`
+				Type            string `json:"type"`
+				Name            string `json:"name,omitempty"`
+				Anonymous       bool   `json:"anonymous,omitempty"`
+				Outputs         []struct {
+					Name         string `json:"name"`
+					InternalType string `json:"internalType"`
+					Type         string `json:"type"`
+				} `json:"outputs,omitempty"`
+			} `json:"abi"`
+			Type                string        `json:"type"`
+			TransactionHash     string        `json:"transactionHash"`
+			CollectionName      string        `json:"collectionName"`
+			ChainId             int           `json:"chainId"`
+			ContractName        string        `json:"contractName"`
+			MaxSupply           int           `json:"maxSupply"`
+			HasWhitelistMinted  []interface{} `json:"hasWhitelistMinted"`
+			Deployed            int64         `json:"deployed"`
+			SoldOut             bool          `json:"soldOut"`
+			CollectionCoverUrl  string        `json:"collectionCoverUrl"`
+			Description         string        `json:"description"`
+			Twitter             string        `json:"twitter"`
+			LaunchDate          int64         `json:"launchDate"`
+			LaunchLater         bool          `json:"launchLater"`
+			MetadataCID         string        `json:"metadataCID"`
+			IsWhitelist         bool          `json:"isWhitelist"`
+			Whitelist           []interface{} `json:"whitelist"`
+			TwitterVerified     bool          `json:"twitterVerified"`
+			CollectionBannerUrl string        `json:"collectionBannerUrl"`
+			Pos                 int           `json:"pos"`
+			Featured            bool          `json:"featured"`
+			HasSoldEnough       bool          `json:"hasSoldEnough"`
+			FractionMinted      float64       `json:"fractionMinted"`
+			LastMintedAt        int64         `json:"lastMintedAt"`
+			TotalMints          int           `json:"totalMints"`
+			MintedLast30Mins    bool          `json:"mintedLast30mins"`
+			HeartCount          int           `json:"heartCount"`
+			StartTime           interface{}   `json:"startTime"`
+		} `json:"collection"`
+		DynamicCtx interface{} `json:"dynamicCtx"`
+	} `json:"pageProps"`
+	NSSP bool `json:"__N_SSP"`
+}
+
+type resAptos struct {
+	PageProps struct {
+		Collection struct {
+			Owner               string `json:"owner"`
+			Cost                string `json:"cost"`
+			Hidden              bool   `json:"hidden"`
+			Cm                  string `json:"cm"`
+			MaxSupply           int    `json:"maxSupply"`
+			LaunchDate          int64  `json:"launchDate"`
+			Type                string `json:"type"`
+			MetadataCID         string `json:"metadataCID"`
+			CollectionName      string `json:"collectionName"`
+			Deployed            int64  `json:"deployed"`
+			Description         string `json:"description"`
+			CollectionCoverURL  string `json:"collectionCoverUrl"`
+			CollectionBannerURL string `json:"collectionBannerUrl"`
+			Discord             string `json:"discord"`
+			HasSoldEnough       bool   `json:"hasSoldEnough"`
+			FractionMinted      int    `json:"fractionMinted"`
+			SoldOut             bool   `json:"soldOut"`
+			TotalMints          int    `json:"totalMints"`
+			LastMintedAt        int64  `json:"lastMintedAt"`
+			MintedLast30Mins    bool   `json:"mintedLast30mins"`
+			Pos                 int    `json:"pos"`
+			Featured            bool   `json:"featured"`
+			Twitter             string `json:"twitter"`
+			TwitterVerified     bool   `json:"twitterVerified"`
+			HeartCount          int    `json:"heartCount"`
+			StartTime           any    `json:"startTime"`
+		} `json:"collection"`
+		DynamicCtx any `json:"dynamicCtx"`
+	} `json:"pageProps"`
+	NSsp bool `json:"__N_SSP"`
+}
+
+type resPolygon struct {
+	PageProps struct {
+		Collection struct {
+			WhitelistCost string `json:"whitelistCost"`
+			Owner         string `json:"owner"`
+			Address       string `json:"address"`
+			Hidden        bool   `json:"hidden"`
+			Abi           []struct {
+				Inputs          []any  `json:"inputs"`
+				StateMutability string `json:"stateMutability,omitempty"`
+				Type            string `json:"type"`
+				Name            string `json:"name,omitempty"`
+				Anonymous       bool   `json:"anonymous,omitempty"`
+				Outputs         []any  `json:"outputs,omitempty"`
+			} `json:"abi"`
+			Type                string  `json:"type"`
+			Version             int     `json:"version"`
+			TransactionHash     string  `json:"transactionHash"`
+			CollectionName      string  `json:"collectionName"`
+			ChainID             int     `json:"chainId"`
+			ContractName        string  `json:"contractName"`
+			MaxSupply           int     `json:"maxSupply"`
+			HasWhitelistMinted  []any   `json:"hasWhitelistMinted"`
+			Deployed            int64   `json:"deployed"`
+			CollectionBannerURL string  `json:"collectionBannerUrl"`
+			CollectionCoverURL  string  `json:"collectionCoverUrl"`
+			LaunchDate          int64   `json:"launchDate"`
+			LaunchLater         bool    `json:"launchLater"`
+			MetadataCID         string  `json:"metadataCID"`
+			SoldOut             bool    `json:"soldOut"`
+			WlMaxMints          int     `json:"wlMaxMints"`
+			HasSoldEnough       bool    `json:"hasSoldEnough"`
+			IsWhitelist         bool    `json:"isWhitelist"`
+			Whitelist           []any   `json:"whitelist"`
+			MaxMints            int     `json:"maxMints"`
+			Description         string  `json:"description"`
+			Twitter             string  `json:"twitter"`
+			TwitterVerified     bool    `json:"twitterVerified"`
+			Discord             string  `json:"discord"`
+			Cost                string  `json:"cost"`
+			HeartCount          int     `json:"heartCount"`
+			MintedLast30Mins    bool    `json:"mintedLast30mins"`
+			FractionMinted      float64 `json:"fractionMinted"`
+			LastMintedAt        int64   `json:"lastMintedAt"`
+			TotalMints          int     `json:"totalMints"`
+			StartTime           any     `json:"startTime"`
+		} `json:"collection"`
+		DynamicCtx any `json:"dynamicCtx"`
+	} `json:"pageProps"`
+	NSsp bool `json:"__N_SSP"`
+}
+
+type resAvalanche struct {
+	Props struct {
+		PageProps struct {
+			Collection struct {
+				ContractHash  string `json:"contractHash"`
+				Type          string `json:"type"`
+				Owner         string `json:"owner"`
+				WhitelistCost any    `json:"whitelistCost"`
+				Address       string `json:"address"`
+				Hidden        bool   `json:"hidden"`
+				Abi           []struct {
+					Inputs          []any  `json:"inputs"`
+					StateMutability string `json:"stateMutability,omitempty"`
+					Type            string `json:"type"`
+					Name            string `json:"name,omitempty"`
+					Anonymous       bool   `json:"anonymous,omitempty"`
+					Outputs         []any  `json:"outputs,omitempty"`
+				} `json:"abi"`
+				Deployed            int64   `json:"deployed"`
+				Whitelist           any     `json:"whitelist"`
+				LaunchLater         bool    `json:"launchLater"`
+				Version             int     `json:"version"`
+				TransactionHash     string  `json:"transactionHash"`
+				CollectionName      string  `json:"collectionName"`
+				WlMaxMints          any     `json:"wlMaxMints"`
+				IsWhitelist         bool    `json:"isWhitelist"`
+				ChainID             int     `json:"chainId"`
+				ContractName        string  `json:"contractName"`
+				MaxSupply           int     `json:"maxSupply"`
+				MetadataCID         string  `json:"metadataCID"`
+				MaxMints            string  `json:"maxMints"`
+				EnforceRoyalties    bool    `json:"enforceRoyalties"`
+				HasWhitelistMinted  any     `json:"hasWhitelistMinted"`
+				CollectionBannerURL string  `json:"collectionBannerUrl"`
+				CollectionCoverURL  string  `json:"collectionCoverUrl"`
+				Twitter             string  `json:"twitter"`
+				TwitterVerified     bool    `json:"twitterVerified"`
+				HeartCount          int     `json:"heartCount"`
+				SoldOut             bool    `json:"soldOut"`
+				HasSoldEnough       bool    `json:"hasSoldEnough"`
+				FractionMinted      float64 `json:"fractionMinted"`
+				LastMintedAt        int64   `json:"lastMintedAt"`
+				TotalMints          int     `json:"totalMints"`
+				MintedLast30Mins    bool    `json:"mintedLast30mins"`
+				Cost                string  `json:"cost"`
+				StartTime           any     `json:"startTime"`
+				LaunchDate          any     `json:"launchDate"`
+			} `json:"collection"`
+			DynamicCtx any `json:"dynamicCtx"`
+		} `json:"pageProps"`
+		NSsp bool `json:"__N_SSP"`
+	} `json:"props"`
+	Page  string `json:"page"`
+	Query struct {
+		Userid       string `json:"userid"`
+		Collectionid string `json:"collectionid"`
+	} `json:"query"`
+	BuildID      string `json:"buildId"`
+	IsFallback   bool   `json:"isFallback"`
+	DynamicIds   []int  `json:"dynamicIds"`
+	Gssp         bool   `json:"gssp"`
+	ScriptLoader []any  `json:"scriptLoader"`
+}
