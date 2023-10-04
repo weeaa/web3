8f9fba8579c0a41c115329745fcc73c39a6b63e7
weeaa <github@ryanriviere.com>
2023-10-05 01:33:24 +0200

    updated db

--- db/commands.go
+++ db/commands.go
@@ -12,11 +12,16 @@ func (pg *DB) InsertUser(u *User) error {
 	return err
 }
 
-func (pg *DB) GetUser(baseAddress string) (*User, error) {
-	return nil, nil
+func (pg *DB) GetUser(baseAddress string) (User, error) {
+	var user User
+	if err := pg.db.Model(&user).Where("base_address = ?", baseAddress).Select(); err != nil {
+		return User{}, err
+	}
+	return user, nil
 }
 
 func (pg *DB) UpdateUser(baseAddress string) error {
+
 	return nil
 }
 
@@ -24,3 +29,19 @@ func (pg *DB) RemoveUser(baseAddress string) error {
 	_, err := pg.db.Model(User{BaseAddress: baseAddress}).WherePK().Delete()
 	return err
 }
+
+func (pg *DB) GetAllUsers() ([]User, error) {
+	var users []User
+	err := pg.db.Model(&users).Select()
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+/*
+func (pg *DB) WipeTable() error {
+	pg.db.Model().Delete()
+	return nil
+}
+*/
