6768674167d796f99d1a12295f2028cbbf7a7cc5
weeaa <github@ryanriviere.com>
2023-12-13 07:58:10 +0100

    Added Prometheus Metrics – test

--- pkg/prometheus/metrics.go
+++ pkg/prometheus/metrics.go
@@ -5,23 +5,49 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rs/zerolog"
+	"github.com/shirou/gopsutil/disk"
 	"net/http"
+	"runtime"
+	"time"
 )
 
 const (
 	DefaultPort = ":2115"
 )
 
-type PromMetrics struct {
-	DatabaseQueryDuration *prometheus.SummaryVec
-
-	GoroutineCount prometheus.Counter
-
-	FriendTechMetrics      FriendTechMetrics
-	EthereumWatcherMetrics EthereumWatcher
+type Metrics struct {
+	logger zerolog.Logger
 }
 
-type DatabaseQueriesDuration struct {
+type PromMetrics struct {
+	DatabaseQueryDuration *prometheus.SummaryVec
+	
+	DatabaseMetrics       DatabaseMetrics
+	InfrastructureMetrics InfrastructureMetrics
+	
+	FriendTechMetrics      FriendTechMetrics
+	EthereumWatcherMetrics EthereumWatcher
+	
+	PanicsRecovered prometheus.Counter
+}
+
+type InfrastructureMetrics struct {
+	GarbageCollection prometheus.Histogram
+	MemoryUsage       prometheus.Gauge
+	CpuUsage          prometheus.Gauge
+	GoroutineCount    prometheus.Gauge
+	DiskOperations    DiskOperations
+}
+
+type DiskOperations struct {
+	Read  prometheus.Gauge
+	Write prometheus.Gauge
+}
+
+type DatabaseMetrics struct {
+	QueryExecutionTime *prometheus.SummaryVec
+	ErrorRate          prometheus.Counter
 }
 
 type EthereumWatcher struct {
@@ -29,6 +55,10 @@ type EthereumWatcher struct {
 
 type FriendTechMetrics struct {
 	BlockchainDataLatency prometheus.Histogram
+}
+
+type UnisatMetrics struct {
+	Latency prometheus.Histogram
 }
 
 func (p *PromMetrics) Initialize(port string) error {
@@ -39,17 +69,47 @@ func (p *PromMetrics) Initialize(port string) error {
 }
 
 func NewPromMetrics() *PromMetrics {
+	
 	return &PromMetrics{
-		DatabaseQueryDuration: promauto.NewSummaryVec(prometheus.SummaryOpts{
-			Name: "db_query_duration_seconds",
-			Help: "Database query execution time in seconds",
+		DatabaseMetrics: DatabaseMetrics{
+			QueryExecutionTime: promauto.NewSummaryVec(prometheus.SummaryOpts{
+				Name: "db_query_duration_ms",
+				Help: "Database query execution time in milliseconds",
+			},
+				[]string{
+					"",
+				},
+			),
 		},
-			[]string{"query_name"},
-		),
-		GoroutineCount: promauto.NewCounter(prometheus.CounterOpts{
-			Name: "",
-			Help: "",
-		}),
+		InfrastructureMetrics: InfrastructureMetrics{
+			GarbageCollection: promauto.NewHistogram(prometheus.HistogramOpts{
+				Name: "garbage_collection_frequency_duration",
+				Help: "Track garbage collection frequency and duration",
+			}),
+			MemoryUsage: promauto.NewGauge(prometheus.GaugeOpts{
+				Name: "memory_usage",
+				Help: "Monitor heap, stack, and system memory usage",
+			}),
+			CpuUsage: promauto.NewGauge(prometheus.GaugeOpts{
+				Name: "cpu_usage",
+				Help: "Monitor the CPU usage of the servers running the application",
+			}),
+			GoroutineCount: promauto.NewGauge(prometheus.GaugeOpts{
+				Name: "goroutine_count",
+				Help: "Number of running goroutines currently running",
+			}),
+			/*
+			NetworkTraffic: promauto.NewCounter(prometheus.CounterOpts{
+				Name: "network_traffic",
+				Help: "Incoming and outgoing network traffic statistics",
+			}),
+			*/
+			DiskOperations: promauto.NewCounter(prometheus.CounterOpts{
+				Name: "disk_operations",
+				Help: "Disk read/write operations statistics",
+			}),
+		},
+		
 		EthereumWatcherMetrics: EthereumWatcher{},
 		FriendTechMetrics: FriendTechMetrics{
 			BlockchainDataLatency: promauto.NewHistogram(prometheus.HistogramOpts{
@@ -58,4 +118,57 @@ func NewPromMetrics() *PromMetrics {
 			}),
 		},
 	}
+}
+
+// MonitorGoRoutinesCount monitors the number of goroutines running in the InfrastructureMetrics instance.
+// It updates the GoroutineCount gauge with the current count of goroutines every second.
+func (i *InfrastructureMetrics) monitorGoRoutinesCount() {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				i.monitorGoRoutinesCount()
+			}
+		}()
+		for {
+			i.GoroutineCount.Set(float64(runtime.NumGoroutine()))
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+// MonitorGarbageCollection monitors the garbage collection statistics of the InfrastructureMetrics instance.
+// It periodically prints the current memory allocation, total allocation, system memory usage, and number of garbage collections.
+
+// I have no clue if it is even useful but here we go ;)
+func (i *InfrastructureMetrics) monitorInfrastructure() {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				i.monitorInfrastructure()
+			}
+		}()
+		for {
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			
+			i.CpuUsage.Set(m.GCCPUFraction)
+			i.GarbageCollection.Observe(float64(m.NumGC))
+			i.MemoryUsage.Set(float64(m.Sys))
+			
+			partitions, _ := disk.Partitions(false)
+			for _, partition := range partitions {
+				ioStat, _ := disk.IOCounters(partition.Mountpoint)
+				for _, io := range ioStat {
+					i.DiskOperations.Read.Set(float64(io.ReadCount))
+					i.DiskOperations.Write.Set(float64(io.WriteCount))
+					//log.Printf("ReadCount: %v, WriteCount: %v\n", io.ReadCount, io.WriteCount)
+				}
+			}
+			time.Sleep(time.Minute)
+		}
+	}()
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
